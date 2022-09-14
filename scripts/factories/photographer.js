f56d3fca0035575e63f8f1e772f6f2c5b694eb11
OrionFE <102513015+OrionFE@users.noreply.github.com>
2022-09-14 17:54:16 +0200

    add description

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,26 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+  const { name, portrait, city, country, tagline, price } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+  const picture = `assets/photographers/${portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+  function getUserCardDOM() {
+    const article = document.createElement("article");
+    const img = document.createElement("img");
+    img.setAttribute("src", picture);
+    const h2 = document.createElement("h2");
+    h2.textContent = name;
+    const location = document.createElement("p");
+    location.innerText = city + ", " + country;
+    const description = document.createElement("p");
+    description.innerText = tagline;
+    const pricetag = document.createElement("p");
+    pricetag.innerText = price + "€/jour";
+    article.appendChild(img);
+    article.appendChild(h2);
+    article.appendChild(location);
+    article.appendChild(description);
+    article.appendChild(pricetag);
+    return article;
+  }
+  return { name, picture, getUserCardDOM };
 }
